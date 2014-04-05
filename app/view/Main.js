55e151a060b1e39837ba785dff8ac0b43d8bb5ed
eliu7 <eliu7@binghamton.edu>
2014-04-05 18:44:06 -0400

    idk mang

--- app/view/Main.js
+++ app/view/Main.js
@@ -10,7 +10,7 @@ Ext.define('touch_gossip.view.Main', {
 
         items: [
             {
-                title: 'Welcome',
+                title: 'Home Tab',
                 iconCls: 'home',
 
                 styleHtmlContent: true,
@@ -23,13 +23,11 @@ Ext.define('touch_gossip.view.Main', {
                 },
 
                 html: [
-                    "You've just generated a new Sencha Touch 2 project. What you're looking at right now is the ",
-                    "contents of <a target='_blank' href=\"app/view/Main.js\">app/view/Main.js</a> - edit that file ",
-                    "and refresh to change what's rendered here."
+                    "I changed the default <b>HTML Contents</b> to something different!"
                 ].join("")
             },
             {
-                title: 'Get Started',
+                title: 'Wohoo',
                 iconCls: 'action',
 
                 items: [
